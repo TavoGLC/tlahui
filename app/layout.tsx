082b9046d337d9ae62442043b3949fa9d28918f7
Octavio Gonzalez-Lugo <ogonzalezl@outlook.com>
2024-01-22 19:40:13 -0600

    Aded Meta Data

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,19 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Tlahuizcalpantecuhtli',
-  description: 'SARS-Cov-2 Forecasting',
+  description: 'SARS-Cov-2 Forecasting with machine learning',
+  openGraph: {
+    title: 'Tlahuizcalpantecuhtli',
+    description: 'SARS-Cov-2 Forecasting forecasting with machine learning',
+    url: 'https://tlahui.vercel.app/',
+    siteName: 'Tlahuizcalpantecuhtli',
+    images: [
+      {
+        url: 'https://tlahui.vercel.app/social-media-image.jpg', // Must be an absolute URL
+        width: 640,
+        height: 427,
+      }]
+  }
 }
 
 export default function RootLayout({
